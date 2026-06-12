2a4b20c2b0e6f6d8d3527fca62c94d9962710fab
ValorieMcFlurry <vexithereal@gmail.com>
2026-06-12 16:20:56 +0100

    Refactor Discord presence initialization and update

--- main.cpp
+++ main.cpp
@@ -5,39 +5,42 @@
 #include <wrl.h>
 #include <wil/com.h>
 #include "WebView2.h"
-
-// Explicitly linking Discord RPC Header (Requires discord-rpc SDK dependency linked)
 #include "discord_rpc.h"
 
 using namespace Microsoft::WRL;
 
-// Global Handle Declarations
 HWND hWnd;
 static TCHAR szWindowClass[] = _T("LarpMediaApp");
 static TCHAR szTitle[] = _T("LarpMedia");
 wil::com_ptr<ICoreWebView2Controller> webviewController;
 wil::com_ptr<ICoreWebView2> webviewWindow;
 
-// Discord Rich Presence Setup Engine
-void InitializeDiscordPresence() {
-    DiscordEventHandlers handlers;
-    memset(&handlers, 0, sizeof(handlers));
-    
-    // Discord RPC
-    Discord_Initialize("1515011986021421218", &handlers, 1, NULL);
-    
+void UpdateDiscordTrackPresence(const char* trackName, const char* durationStr) {
     DiscordRichPresence discordPresence;
     memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Being a Larp God";
-    discordPresence.details = "Larping";
-    discordPresence.largeImageKey = "larpmedia";
+    
+    // Explicitly mapping the activity type: 
+    // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching
+    discordPresence.activityType = 2; 
+    
+    discordPresence.state = trackName;
+    discordPresence.details = "Processing Harmonics";
+    discordPresence.largeImageKey = "larpmedia_purple";
     discordPresence.largeImageText = "LarpMedia";
     discordPresence.instance = 1;
     
     Discord_UpdatePresence(&discordPresence);
 }
 
-// Window Event Messaging Procedure
+void InitializeDiscordPresence() {
+    DiscordEventHandlers handlers;
+    memset(&handlers, 0, sizeof(handlers));
+    
+    // Initializing with your custom Discord Application ID
+    Discord_Initialize("1515011986021421218", &handlers, 1, NULL);
+    UpdateDiscordTrackPresence("Idle / Deck Empty", "0:00");
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
         case WM_SIZE:
@@ -58,10 +61,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 }
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // 1. Initialize Discord Status Thread
     InitializeDiscordPresence();
 
-    // 2. Class Registration
     WNDCLASSEX wcex;
     wcex.cbSize = sizeof(WNDCLASSEX);
     wcex.style = CS_HREDRAW | CS_VREDRAW;
@@ -76,11 +77,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     wcex.lpszClassName = szWindowClass;
     wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 
-    if (!RegisterClassEx(&wcex)) {
-        return 1;
-    }
+    if (!RegisterClassEx(&wcex)) return 1;
 
-    // 3. Create Desktop Frame Window
     hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 
                         1300, 850, NULL, NULL, hInstance, NULL);
 
@@ -89,7 +87,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
-    // 4. Fire Up Edge Chromium Instance
     CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
         Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
             [](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
@@ -102,20 +99,17 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                         RECT bounds;
                         GetClientRect(hWnd, &bounds);
                         webviewController->put_Bounds(bounds);
-
-                        // Points execution straight to the application's UI file
                         webviewWindow->Navigate(L"file:///index.html");
                         return S_OK;
                     }).Get());
                 return S_OK;
             }).Get());
 
-    // 5. Windows Event Execution Loop
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
-        Discord_RunCallbacks(); // Tick Discord event loop
+        Discord_RunCallbacks();
     }
     return (int)msg.wParam;
 }
